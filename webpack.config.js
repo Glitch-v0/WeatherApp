714f02e034f99a170ef6bd2d6ed838c82853d5be
Nathan Jones <njonessurvey@gmail.com>
2023-10-24 11:49:19 -0500

    Initial Project Setup

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@
  const webpack = require('webpack');
 
  module.exports = {
-   entry: './src/script.js',
+   entry: '/src/script.js',
    devtool: 'inline-source-map',
    devServer: {
       static: './dist',
